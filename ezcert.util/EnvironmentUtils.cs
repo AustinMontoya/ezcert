0e1235b150188cd2ea6c7486a6c697346ead7b46
grrizzly <austin.r.montoya@gmail.com>
2016-03-02 19:13:11 -0500

    added security section unlock function

---
+++ ezcert.util/EnvironmentUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace ezcert.util
+{
+  public class EnvironmentUtils
+  {
+    public static void UnlockConfigSection(string path)
+    {
+      if (string.IsNullOrEmpty(path))
+      {
+        path = FindApplicationHostConfig(Environment.CurrentDirectory);
+        if (path == null) throw new InvalidOperationException("No applicationhost.config found in a .vs or documents folder");
+      }
+      
+      var doc = XDocument.Load(path);
+      var element = doc.XPathSelectElement("//sectionGroup[@name='security']/section[@name='access']");
+      element.SetAttributeValue(XName.Get("overrideModeDefault"), "Allow");
+      doc.Save(path);
+    }
+
+ 
+    private static string FindApplicationHostConfig(string path)
+    {
+      if (path == Path.GetPathRoot(path))
+      {
+        var globalConfigPath = Path.Combine(path, "Users", Environment.UserName, "Documents", "IISExpress", "config", "applicationhost.config");
+        return File.Exists(globalConfigPath) ? globalConfigPath : null;
+      }
+
+      var localConfigPath = Path.Combine(path, ".vs", "config", "applicationhost.config");
+      return File.Exists(localConfigPath)
+        ? localConfigPath
+        : FindApplicationHostConfig(Directory.GetParent(path).FullName);
+    }
+  }
+}
---
+++ powershell/Unlock-AppHostSecuritySection.ps1
@@ -0,0 +1,35 @@
+﻿Add-Type -Path 'ezcert.util.dll'
+
+function Unlock-AppHostSecuritySection {
+    Param(
+        [string]$Path
+    )
+
+    if (!$Path) {
+        Write-Host "Searching for applicationhost.config..."
+        $Path = Get-AppHostConfigPath((Get-Item -Path $PWD))
+    }
+
+    Write-Host "Modifying $Path"
+    [ezcert.util.EnvironmentUtils]::UnlockConfigSection($Path)
+    Write-Host "Config section unlocked"
+}
+
+function Get-AppHostConfigPath ($path) {
+    if ($path.ToString() -eq [IO.Path]::GetPathRoot($path)) {
+        Write-Host "Local applicationhost.config not found, modifying machine-wide config"
+        $globalConfigPath = [IO.Path]::Combine($path, "Users", $env:USERNAME, "Documents/IISExpress/config/applicationhost.config")
+        if (Test-Path $globalConfigPath) {
+            return $globalConfigPath
+        }
+
+        throw "No applicationhost.config found in .vs/config anywhere in this directory or its parents, or in ~/Documents/IISExpress/config"    
+    }
+
+    $localConfigPath = [IO.Path]::Combine($path, ".vs", "config", "applicationhost.config")
+    if (Test-Path $localConfigPath) {
+        return $localConfigPath
+    }
+
+    return Get-AppHostConfigPath -path (Get-Item $path).Parent.FullName
+}
